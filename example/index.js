e023af2fad31c52ddedc153076b7acad2edaf744
nem0z <maxime.bernard.dev@gmail.com>
2023-01-11 23:40:01 +0100

    feat (example): remove example folder (not updated)

--- example/index.js
+++
@@ -1,9 +0,0 @@
-import Client from '../client.js';
-import config from './config.js';
-
-const client = Client(config);
-const block_hash = "344fe7927e6a3723b21ec0ec6b9f31d722ac03d667dd10d209b00047380370bb";
-
-client.getBlock(block_hash)
-    .then(block => console.log("Block retrived :\n", block))
-    .catch(console.error);
